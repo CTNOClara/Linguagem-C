f4bdb49aca48fa65e3ad2c5d7c0c2e1c3c7d8ca6
Clarissa <ctndo@icomp.ufam.edu.br>
2022-07-06 14:18:05 -0400

    Arquivos Linguagem C Arv 2-3-4

---
+++ MARCAÇÃO DE CONSULTA EM UMA CLÍNICA VETERINÁRIAA.c
@@ -0,0 +1,385 @@
+/*
+04/2022 (PT-BR) - Manaus/AM
+
+Universidade Federal do Amazonas - UFAM
+Algoritmo e Estrutura de Dados 2 - AED2
+
+Trabalho Prático 02 - Marcação de Consulta em Clínica Veterinária (Árvore 2-3-4)
+Autora: Clarissa Thaís Nascimento de Oliveira 
+*/
+
+#include "math.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdbool.h"
+
+#include "biblioteca.h"
+//função para criar arvore/inicicalizar arvore
+TNO* criarArvore(){ 
+TNO* Arvore = (TNO*) malloc(sizeof(TNO));
+
+    Arvore->chave[0] = 0;
+    Arvore->filho[0] = NULL;
+    Arvore->filho[1] = NULL;
+    Arvore->filho[2] = NULL;
+    Arvore->filho[3] = NULL;
+
+return Arvore;
+}
+//inicializar pilha
+TPilha* criarPilha(){
+    TPilha* Pilha = (TPilha*) malloc(sizeof(TPilha));
+    Pilha->pai = NULL;
+    return Pilha;
+}
+
+TNO *criarNo(long int chave) { //cria/inicializa novo nó
+    TNO *novoNo;
+    
+    novoNo = criarArvore();
+    novoNo -> chave[0] = 1;
+    novoNo -> chave[1] = chave;
+
+    return novoNo;
+}
+//funções que serão tratadas para o processo de inserção
+TNO *inserirNo(TNO *pai, TNO *no, int indiceice) {
+    pai -> filho[indiceice] = no;
+
+    return pai;
+}
+
+TNO *inserirOrdenado(TNO *no, long int chave) {
+    long int aux = 0;
+    long int meio = (no -> chave[0] / 2) + 1; 
+
+    if(chave < no->chave[meio]) {
+        if(chave < no->chave[meio - 1] && (meio-1) != 0) {
+            aux = no -> chave[meio];
+            no -> chave[meio+1] = aux;
+            aux = no -> chave[meio-1];
+            no -> chave[meio-1] = chave; 
+            no -> chave[meio] = aux;
+            no -> chave[0]++;
+        } else {
+            aux = no -> chave[meio];
+            no -> chave[meio] = chave; 
+            no -> chave[meio+1] = aux;
+            no -> chave[0]++;
+        }
+    } else {
+        no -> chave[meio+1] = chave;
+        no -> chave[0]++;
+        
+    }
+    return no;
+}
+
+bool ehFolha(TNO *no) { 
+    if(no->filho[0] == NULL && no->filho[1] == NULL && no->filho[2] == NULL && no->filho[3] == NULL) {                     
+        return true; // eh folha
+    } else {
+        return false; // nao eh folha
+    }
+}
+
+bool existe(TNO *no, long int chave) { 
+    int aux = pertence(no, chave);
+    
+    if(aux == 0){ // não existe
+        return  0;
+    } else if(aux > 0 && aux < 5){ // existe
+        return 1;
+    } else {
+       return existe(no->filho[aux-5], chave);
+    }
+}
+
+bool pilhaVazia(TPilha *pilha) { 
+    if(pilha->pai == NULL) {
+        return true;
+    }else{
+    	return false;
+	}  
+}
+
+bool arvoreVazia(TNO *no, TPilha *pilha) { 
+    if(pilhaVazia(pilha) && no->chave[0] == 0) {
+        return true;//arvore vazia
+    }
+    
+    return false;
+}
+
+int temEspaco(TNO *no) { 
+    if(no->chave[0] == 3) { // não tem espaco
+        return 0;
+    } else { // tem espaco
+        return 1;
+    }
+}
+
+int pertence(TNO *no, long int chave) {
+    int filho = 5;
+    int indice = 1;
+    
+    while(indice <= no->chave[0]) {
+        if(chave == no->chave[indice]) {
+            return indice;
+        }
+
+        if(chave < no->chave[indice] && no->filho[0] != NULL) { 
+            return filho;
+        }
+
+        if(chave > no->chave[indice] && no->filho[0] != NULL && indice == no->chave[0]) { 
+            return filho + 1;
+        }
+
+        filho++;
+        indice++;
+    }
+
+    return 0;
+}
+
+int identificarFilho(TNO *no, long int chave) {
+    int aux = pertence(no, chave);
+    
+    if(aux != 0) { aux -= 5; }
+
+    return aux;
+}
+
+TNO *topoPilha(TPilha *pilha) {
+    return (pilha -> pai);
+}
+
+TNO *split(TNO *no, TPilha *pilha) { 
+   int pos;
+   TNO *pai, *irmao;
+
+    // insere a chave[2](central) no pai
+    if(pilhaVazia(pilha)){ // quebra a raiz
+        pai = criarNo(no->chave[2]);
+        pai -> filho[0] = no;
+    } else { 
+        pai = topoPilha(pilha);
+        pilha  = removerTopo(pilha);
+        pai = inserirInterno(pai, no, pilha);
+    }
+
+    // cria irmão e coloca seus filhos 
+    irmao = criarNo(no->chave[3]);
+    irmao -> filho[0] = no -> filho[2]; 
+    irmao -> filho[1] = no -> filho[3];
+
+    // organiza filhos (removendo os filhos 2 e 3)
+    no -> filho[2] = NULL;
+    no -> filho[3] = NULL;
+    no -> chave[0] = 1;
+
+    // usa pertence para dizer onde inserir o filho
+    pos = pertence(pai, irmao->chave[1]); 
+    pai = inserirNo(pai, irmao, (pos-5));
+
+    //insere o pai modificado na pilha
+    pilha = inserirPilha(pilha, pai); 
+
+    return pai;
+}
+
+TNO *inserir(TNO *raiz, long int chave, TPilha *pilha) {
+    if(arvoreVazia(raiz, pilha)) {
+        raiz = criarNo(chave);
+
+        return raiz;
+    }
+    
+    if(!existe(raiz, chave)) { // se não existe
+        if(ehFolha(raiz) == 1){ // se é folha
+            if(temEspaco(raiz) == 1){ // se tem espaço 
+                raiz = inserirOrdenado(raiz, chave); 
+
+                return raiz;
+            } else { // não tem espaço 
+                raiz = split(raiz, pilha); 
+                raiz = inserir(raiz, chave, pilha); 
+
+                return raiz;
+            }
+        } else { // se não é folha
+            pilha = inserirPilha(pilha, raiz); 
+            
+            TNO *filho = criarArvore();
+            int idFilho = identificarFilho(raiz, chave); // descobre o filho que deve ser inserido
+            
+            filho = raiz -> filho[idFilho]; 
+            filho = inserir(filho, chave, pilha); // insere no filho 
+
+            return raiz;
+        }
+    } else { // existe
+        return raiz;
+    }
+}
+
+TNO *inserirInterno(TNO *pai, TNO *no, TPilha *pilha) {
+    if(temEspaco(pai)) {
+        pai = inserirOrdenado(pai, no->chave[2]);
+    } else {
+        pai = split(pai, pilha);
+    }
+    return pai;
+}
+
+TPilha *inserirPilha(TPilha *topo, TNO *no) { 
+    TPilha *aux = criarPilha();
+
+    aux -> pai = no;
+    aux -> prox = topo;
+
+    return aux;
+}
+
+TPilha *removerTopo(TPilha *topo) { 
+    TPilha *novoTopo;
+    novoTopo = topo -> prox;
+    free(topo);
+
+    return novoTopo;
+}
+
+//funções que serão tratadas para a remoção de elementos da arvore
+int buscar(TNO *no) {
+    int aux = no -> chave[0];
+
+    if (ehFolha(no)) { // é folha
+        return no -> chave[aux];
+    } else { // não é folha
+        return buscar(no->filho[aux]);
+    }
+}
+
+TNO *juntar(TPilha *pilha, int aux) {
+    pilha -> pai = inserirOrdenado(pilha->pai, pilha->pai->filho[aux]->chave[1]);
+    pilha -> pai -> filho[0] = NULL;
+    pilha -> pai -> filho[1] = NULL;
+
+    return pilha;
+}
+
+//ROTAÇÕES
+TNO *RotacaoDireita(TNO *pai, int aux) { 
+
+}
+
+TNO *RotacaoEsquerda(TNO *pai, int aux) {
+    int indice = pai -> filho[aux] -> chave[0];
+    
+    pai -> chave[1] = pai -> chave[1];
+    pai -> chave[1] = pai -> filho[aux] -> chave[indice];
+    pai -> filho[aux] -> chave[0] = removerChaveNo(pai->filho[aux], pai->filho[aux]->chave[indice]);
+    
+    return pai;
+}
+
+TNO *removerChaveNo(no, chave) {
+
+}
+
+TNO *remover(TNO *no, int chave, TPilha *pilhaPai) {
+    int aux;
+    TNO *filho, *noMDM;
+
+    if(arvoreVazia(no, pilhaPai)) {
+        return no;
+    }
+
+    aux = pertence(no, chave);
+
+    if(aux > 0) { // se a chave está no nó
+        if(ehFolha(no, pilhaPai) && no->chave[0] == 1){ // só tem uma folha em um único nó
+            no = removerChaveNo(no, chave);
+
+            return no;
+        }
+
+        if(!ehFolha(no, pilha)){ // nó intermediario
+            filho = identificarFilho(no, aux);
+            pilhaPai = inserirPilha(pilhaPai, no);
+            noMDM = buscar(filho);
+
+            int indice = noMDM->chave[0];
+            no -> chave[aux] = noMDM -> chave[indice];
+            noMDM = remover(noMDM, noMDM->chave[indice], pilhaPai);
+        }
+
+        if(ehFolha(no)) {
+            int temp = pertence(no, chave);
+            int aux = temp +1; // irmão 
+
+            if(pilhaPai->pai->chave[0] == 1) { // 2 filhos
+                if(no->chave[0] == 1 && pilha->pai->filho[aux]->chave[0] == 1) {
+                    pilhaPai -> pai = juntar(pilhaPai->pai, aux);
+                }
+            }
+        } else { // não tem 1 filho, tem 2
+            if(aux == 0) {
+                pilhaPai -> pai = RotacaoEsquerda(pilhaPai->pai, aux);
+            } else {
+                pilhaPai -> pai = RotacaoDireita(pilhaPai->pai, aux);
+            }
+        }
+    }
+    return no;
+}
+int main() {
+  int num;
+  
+    TNO* Arv = criarArvore();
+    TPilha* Pilha = criarPilha();
+     printf("*CLINICA VETERINÁRIA*\n");
+	printf("Bem vindo ao Sistema de Agendamento de Consultas do seu PET<3\n-Obs:As consultas deverão ser marcadas com ate 24 horas de antecedência!\n");
+	printf("-As consultas vão de 8h até às 19h. E os atendimentos vão de 2a a sábado.\n\n");
+	printf("Digite o número da opção desejada:\n");
+	int codigo;
+	printf("1 - Agendar Consulta\n");
+	printf("2 - Remarcar Consulta\n");
+	printf("3 - Cancelar Consulta\n");
+	scanf("%d",&codigo);
+	
+	int dataehora;
+	char nomedopet[10];
+	switch(codigo){
+		case 1:
+			printf("Diga qual dia e horário você deseja marcar a consulta:");
+			scanf("%d",&dataehora);
+			printf("Qual o nome do seu PET?:");
+			scanf("%s",&nomedopet);
+			//verfica se ta disponivel, se não estiver insere na arvore
+			Arv = inserir(Arv, dataehora, Pilha); 
+			//se não estiver disponivel escolha uma nova data
+			printf("\nO agendamento do(a) %s foi concluido com sucesso.",nomedopet);
+		break;
+		
+		case 2:
+			printf("Para qual dia a consulta estaria marcada?");
+			//salva a informação e faz a remoção e balancemento da arvore.
+			printf("Diga qual dia e horário você deseja remarcar a consulta:");
+			scanf("%d",&dataehora);
+			//verfica se ta disponivel, se não estiver insere na arvore
+			Arv = inserir(Arv, dataehora, Pilha); 
+			//se não estiver disponivel escolha uma nova data
+			printf("Seu agendamento foi concluido com sucesso.");
+		break;
+		
+		case 3:
+			printf("Para qual dia a consulta estaria marcada?");
+			//salva a informação e faz a remoção e balancemento da arvore.
+			scanf("%d",&dataehora);
+			//fazer remoção da chave
+			printf("Sua consulta foi cancelada");
+		break;
+  }
+}
